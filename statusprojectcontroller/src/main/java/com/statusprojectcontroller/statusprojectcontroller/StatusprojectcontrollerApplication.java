48af3538396adc4b94c097d6836f85320b863a0e
rafaelcitario <contato.rafaelgomes@outlook.com>
2023-10-13 23:30:44 -0300

    Init project

---
+++ statusprojectcontroller/src/main/java/com/statusprojectcontroller/statusprojectcontroller/StatusprojectcontrollerApplication.java
@@ -0,0 +1,13 @@
+package com.statusprojectcontroller.statusprojectcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StatusprojectcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StatusprojectcontrollerApplication.class, args);
+	}
+
+}
---
+++ statusprojectcontroller/src/test/java/com/statusprojectcontroller/statusprojectcontroller/StatusprojectcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.statusprojectcontroller.statusprojectcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StatusprojectcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
